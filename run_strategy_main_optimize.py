f02a69e3bad27a106aa77638983f9114e6c918f1
Kabelo Junior Mosaka <kabelojunior10@gmail.com>
2025-07-31 07:07:39 +0200

    algorithm_reversal

--- run_strategy_main_optimize.py
+++ run_strategy_main_optimize.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 from constants2 import LOG_FILE, tickers_all
 from customizable.strategy_params import StrategyParams features.f_v1_basic import add_features_v1_basic
-fr.all_tickersom strategy import run_all_tickers
+from strategy.all_tickers import run_all_tickers
 from utils.local_data import TickersData
 
 logging.basicConfig(
